97994c3971820b5823b27f2cc735871a29a16055
maxxi <129664117+maxxi02@users.noreply.github.com>
2025-04-09 20:40:01 +0800

    fixed env to production

--- dist/common/utils/bcrypt.js
+++ dist/common/utils/bcrypt.js
@@ -4,8 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.compareValue = exports.hashValue = void 0;
-const bcrypt_1 = __importDefault(require("bcrypt"));
-const hashValue = async (value, saltRounds = 10) => await bcrypt_1.default.hash(value, saltRounds);
+const bcryptjs_1 = __importDefault(require("bcryptjs"));
+const hashValue = async (value, saltRounds = 10) => await bcryptjs_1.default.hash(value, saltRounds);
 exports.hashValue = hashValue;
-const compareValue = async (value, hashedValue) => await bcrypt_1.default.compare(value, hashedValue);
+const compareValue = async (value, hashedValue) => await bcryptjs_1.default.compare(value, hashedValue);
 exports.compareValue = compareValue;
