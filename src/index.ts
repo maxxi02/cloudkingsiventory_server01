e16a8cc6c7c4cb90a124fc4f9e39029d00692ec4
maxxi <129664117+maxxi02@users.noreply.github.com>
2025-04-12 17:48:12 +0800

    option

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,8 @@ const corsOptions = {
       ? 'https://ckinventory.vercel.app'
       : 'http://localhost:3000',
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
 };
 
 app.use(cors(corsOptions));
